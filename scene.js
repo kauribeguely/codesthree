5633ff2fdfb4665da3b6f7944410a90e379be9b0
kauribeguely <kauri_che@hotmail.com>
2025-02-08 20:15:52 +1000

    loops working in front end

--- scene.js
+++ scene.js
@@ -24,14 +24,16 @@ export function initializeThreeJsScene(sceneData, containerId)
     const mouseRotationY = sceneData.mouseRotationY; // Maximum rotation range in degrees
     const mouseRotationZ = sceneData.mouseRotationZ; // Maximum rotation range in degrees
     let mouseAnimationLink = sceneData.mouseAnimationLink === 'on';
+
     let scrollAnimationLink = sceneData.scrollAnimationLink === 'on';
     let isOrthoCamera = sceneData.isOrthoCamera === 'on';
+    let loopActive = sceneData.loopActive === 'on';
 
     const scene = new THREE.Scene();
     let camera;
     const perspectiveCamera = new THREE.PerspectiveCamera(50, container.clientWidth / container.clientHeight, 0.1, 1000);
 
-    let isoZoom = 100;    
+    let isoZoom = 100;
     const orthoCamera = new THREE.OrthographicCamera( container.clientWidth / - isoZoom, container.clientWidth / isoZoom, container.clientHeight / isoZoom, container.clientHeight / - isoZoom, 1, 1000 );
 
     if(isOrthoCamera)
@@ -82,14 +84,26 @@ export function initializeThreeJsScene(sceneData, containerId)
     scene.add(alight);
 
 
-    let model;
-
+    let model, loopGroup, loopable;
+    let spacing = sceneData.itemSpacing;
+    let fullLoopGroup = new THREE.Group();
+    let objGroup = new THREE.Group();
+    fullLoopGroup.add(objGroup);
+    scene.add(fullLoopGroup);
 
     // Load 3D Model
     // const loader = new THREE.GLTFLoader();
     const loader = new GLTFLoader();
 
-    loadModel(sceneData.modelUrl, sceneData);
+
+    if(loopActive)
+    {
+      sceneDataLoop();
+    }
+    else
+    {
+      loadModel(sceneData.modelUrl, sceneData);
+    }
     // loadModel('http://localhost/wPpractice/wp-content/uploads/2025/01/first-room.glb', sceneData);
 
     function loadModel(url, sceneData)
@@ -113,9 +127,9 @@ export function initializeThreeJsScene(sceneData, containerId)
 
           model.scale.set(sceneData.scale, sceneData.scale, sceneData.scale);
 
+          if(mouseAnimationLink) window.addEventListener('mousemove', onMouseMove);
 
           renderer.render(scene, camera);
-          if(mouseAnimationLink) window.addEventListener('mousemove', onMouseMove);
           applyScrollTransforms();
         });
     }
@@ -140,9 +154,20 @@ export function initializeThreeJsScene(sceneData, containerId)
       // Map mouse position to rotation range
       // model.rotation.x = THREE.MathUtils.degToRad(initialRotationX + -mouseY * rotationRange);
       // model.rotation.y = THREE.MathUtils.degToRad(initialRotationY + -mouseX * rotationRange);
-      model.rotation.x = THREE.MathUtils.degToRad(initialRotationX + -mouseY * mouseRotationX);
-      model.rotation.y = THREE.MathUtils.degToRad(initialRotationY + -mouseX * mouseRotationY);
-      model.rotation.z = THREE.MathUtils.degToRad(initialRotationX + -mouseX * mouseRotationZ);
+
+      if(loopActive)
+      {
+        fullLoopGroup.rotation.x = THREE.MathUtils.degToRad(initialRotationX + -mouseY * mouseRotationX);
+        fullLoopGroup.rotation.y = THREE.MathUtils.degToRad(initialRotationY + -mouseX * mouseRotationY);
+        fullLoopGroup.rotation.z = THREE.MathUtils.degToRad(initialRotationZ + -mouseX * mouseRotationZ);
+      }
+      else
+      {
+        model.rotation.x = THREE.MathUtils.degToRad(initialRotationX + -mouseY * mouseRotationX);
+        model.rotation.y = THREE.MathUtils.degToRad(initialRotationY + -mouseX * mouseRotationY);
+        model.rotation.z = THREE.MathUtils.degToRad(initialRotationX + -mouseX * mouseRotationZ);
+      }
+
       // console.log(initialRotationX + mouseY * rotationRange);
     };
 
@@ -217,4 +242,128 @@ export function initializeThreeJsScene(sceneData, containerId)
     //     renderer.render(scene, camera);
     // }
     // animate();
+
+    //LOOP CODE
+    function sceneDataLoop()
+    {
+      loopDat(sceneData.modelUrl, sceneData.scale, sceneData.loopCountY, sceneData.loopCountX, sceneData.loopCountZ, objGroup, [sceneData.itemSpacing, 0, 0]);
+      transformObjectToSceneData(fullLoopGroup);
+    }
+
+    function loopDat(objectUrl, objScale, rowCount, columnCount, zCount, group, distances)
+    {
+      // loader.load('obj/laptopIso.glb',	function ( gltf )
+      // loader.load('obj/tabletIso.glb',	function ( gltf )
+      loader.load(objectUrl,	function ( gltf )
+      {
+        // deskObj
+        loopable = gltf.scene;
+        loopable.scale.set(objScale, objScale, objScale);
+        zLoop(loopable, rowCount, columnCount, zCount, group, distances);
+        if(mouseAnimationLink) window.addEventListener('mousemove', onMouseMove);
+        
+      });
+    }
+
+    function zLoop(object, rowCount, columnCount, zCount, group, distances)
+    {
+      for(let i = 0; i < zCount; i++)
+      {
+        let zPlane = new THREE.Group();
+        let zAdjust = zCount/2 - 0.5;
+
+        zPlane.position.set(0, 0, i*sceneData.itemSpacing - zAdjust*sceneData.itemSpacing);
+        group.add(zPlane);
+        rowLoopGroup(loopable, rowCount, columnCount, zCount, zPlane, distances);
+
+
+      }
+    }
+
+    function rowLoopGroup(object, rowCount, columnCount, zCount, group, distances)
+    {
+      for(let i = 0; i < rowCount; i++)
+      {
+        let row = new THREE.Group();
+        let center = i - (0.5*rowCount);
+        let iPercent = i/rowCount;
+        // row.position.set(0.5 * center, 2 * center, 0);
+        // row.position.set(0, center, 0);
+        //TODO row offset
+
+        // let xSpace = 0.5*spacing;
+        // // let xSpace = 0.8*spacing;
+        // if(i % 2 == 1)
+        // {
+        //   xSpace = 0;
+        // }
+        // row.position.set(xSpace, 1*i*spacing, 0);
+
+
+        // row.position.set(xSpace, 0.5*i*spacing, 0);
+        let yAdjust = rowCount/2 - 0.5;
+        row.position.set(0, i*sceneData.itemSpacing - yAdjust*sceneData.itemSpacing, 0);
+        // row.position.set(0, (iPercent-0.5)*(sceneData.itemSpacing*rowCount), 0);
+
+        //just normal add distance between row no centering
+        // row.position.set(0, i*sceneData.itemSpacing, 0);
+
+        // row.position.set(0.5*center, center, 0);
+        // scene.add(row);
+        group.add(row);
+        // loopCreate(object, 10, [1, 0, 1], row); //good for screen
+        loopCreate(object, columnCount, distances, row);
+        // loopCreate(object, 100, [0, 0, 0.5], row);
+
+      }
+
+    }
+
+    function loopCreate(loopObject, loopCount, distances, group)
+    {
+      //todo, dont loop inside a template
+      // let addToDiv = activeDiv;
+      // let loopCount = 20;
+      let randomMax = 1;
+      for(let i = 0; i < loopCount; i++)
+      {
+        let centerMath = i-(0.5*loopCount);
+        let y = centerMath * distances[0];
+
+        //to go slightly off grid
+        let xRandomness = (Math.random() - 0.5) * randomMax;
+        let yRandomness = (Math.random() - 0.5) * randomMax;
+        // console.log('calc: '+centerMath);
+        // activeDiv = addToDiv; //should be the first one
+        // activeDivObj = null;
+        // addObj(loopObject);
+        let loopedObject = loopObject.clone();
+
+        //Screens
+        // loopedObject.position.set(y, centerMath * distances[1], centerMath * distances[2]);
+        loopedObject.position.set(y, centerMath * distances[1], centerMath * distances[2]);
+
+        // Randomed
+        // loopedObject.position.set(y + xRandomness, centerMath * distances[1] + yRandomness, centerMath * distances[2]);
+        // selectedObj.setPosition(centerMath * distances[0], centerMath * distances[1], centerMath * distances[2]);
+        group.add(loopedObject);
+        // starGroup.push(loopedObject);
+      }
+    }
+
+    function transformObjectToSceneData(object)
+    {
+      object.position.set(
+          parseFloat(sceneData.positionX),
+          parseFloat(sceneData.positionY),
+          parseFloat(sceneData.positionZ)
+      );
+
+      object.rotation.set(
+          parseFloat(THREE.MathUtils.degToRad(sceneData.rotationX)),
+          parseFloat(THREE.MathUtils.degToRad(sceneData.rotationY)),
+          parseFloat(THREE.MathUtils.degToRad(sceneData.rotationZ))
+      );
+    }
+
 }
